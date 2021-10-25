9ed68462028c11c09ac94791a9ee48a985981f46
0002sejwal <74792872+0002sejwal@users.noreply.github.com>
2021-10-25 22:40:20 +0530

    stairs.c
    Remember how we studied about 1’s and 0’s? They formulate the native language, the binary number system.
    
    Suppose we need 6 stairs, draw something similar to this.
    
    01 0101 010101 01010101 0101010101 010101010101

---
+++ stairs.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i=0;
+    int j=0;
+    int k=0,l=0;
+    for(i=1;i<7;i++)
+    {
+        for(j=1;j<=i;j++)
+        {            
+            printf("01");
+        }
+
+        printf("\n");
+    }
+}
